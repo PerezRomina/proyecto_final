603aecf430666fc6b684d305c90eb76251063c07
Pérez Romina <mimiromx@gmail.com>
2025-06-02 19:53:51 -0600

    Update programa.py
    turnos,registro, e impresion de datos

--- programa.py
+++ programa.py
@@ -9,18 +9,23 @@ class Aplicacion:
     self.root.geometry("600x700")
     self.root.config(bg="lightblue")
 
+    self.datos_trabajadores[]
+    
     self.menu_lateral= tk.Frame(self.root,bg="light salmon",width=10)
-    self.menu_lateral.pack(side="right",expand=True,fill="both")
-    self.crear_menu
-    self.saludo
+    self.menu_lateral.pack(side="left",fill="y")
+
+    self.area_dinamica = tk.Frame(self.root, bg="bisque")
+    self.area_dinamica.pack(side="right", expand=True, fill="both")
+    self.crear_menu()
+    self.saludo()
 
   
   def crear_menu(self):
     tk.Button(self.menu_lateral,text="INICIO",command=self.saludo,widht=18).pack(pady=10)
     tk.Button(self.menu_lateral, text="REGISTRO",command= self.registro, widht=18).pack(pady=10)
-    tk.Button(self.menu_lateral, text="PUESTOS", command=self.puestos, widht=18).pack(pady=10)
-    tk.Button(self.menu_lateral, text="ASISTENCIAS", command=self.asistecias, widht=18).pack(pady=10)
-    tk.Button(selfmenu_lateral, text="PERIODO VACACIONAL", command=self.periodo, widht=18).pack(pady=10)
+    tk.Button(self.menu_lateral, text="TURNO", command=self.turno, widht=18).pack(pady=10)
+    tk.Button(self.menu_lateral, text="FALTAS", command=self.faltas, widht=18).pack(pady=10)
+    tk.Button(selfmenu_lateral, text="IMPRESION", command=self.impresion, widht=18).pack(pady=10)
 
   
   def limpiar_area_dinamica(self):
@@ -29,108 +34,86 @@ class Aplicacion:
        
   def saludo(self):
      self.limpiar_area_dinamica()
-     tk.Label(self.area_dinamica, text="Aquí va el mensaje de bienvenida", font("Arial",14)).pack(pady=10)
-     tk.Button(self.area_dinamica, text="Mosatrar mensaje de bienvenida")
-          command=lambda:messagebox.showinfo("Bienvenido", "Hola bienvenida")).pack()
+     tk.Label(self.area_dinamica, text="CONTROL ASISTENCIAS", font("Arial",17)).pack(pady=10)
+     tk.Button(self.area_dinamica, text="BIENVENIDA")
+          command=lambda:messagebox.showinfo("Buen dia!", "Bienvenido al sistema de control de asistencias")).pack()
+
+  
     
   def trabajador(self)
      self.limpiar_area_dinamica()
      tk.Label(self.area_dinamica, text="Registro de los datos del trabajador", font=("Arial", 12)).pack(pady=10)
+
      tk.Label(self.area_dinamica, text="Nombre:").pack()
-     nombre = tk.Entry(self.area_dinamica)
+     nombre_entry = tk.Entry(self.area_dinamica)
      nombre.pack(pady=5)
-
-   
-  
     
+     tk.Label(self.area_dinamica, text="Número de trabajador:").pack()
+     num_trab_entry = tk.Entry(self.area_dinamica)
+     num_trab_entry.pack()
+
+     tk.Label(self.area_dinamica, text="Seleccione su género:").pack()
+     genero_combo = ttk.Combobox(self.area_dinamica, values=["Masculino", "Femenino"])
+     genero_combo.pack()
+  def guardar_datos():
+     datos={
+         "nombre": nombre_entry.get(),
+         "edad": edad_entry.get(),
+         "numero": num_trab_entry.get(),
+         "genero": genero_combo.get()
+     }
+     tk.Button(self.area_dinamica, text="Guardar", command=guardar_datos).pack(pady=10)
 
   
-  print("CONTROL DE AISTENCIAS DE TRABAJADOR")
-  print("--Registre los datos del trabajador")
-  nombre=input("Inrese el nombre del trabajador \n")
-  control=input("Ingrese el numero de control \n")
-  edad=input("Ingresa la edad del trabajador \n")
-  genero=input("Ingresa el genero del trabajador \n")
 
-  print("\n ---Puestos---")
-  print("1)Doctor")
-  print("2)Pediatra")
-  print("3)Enfermer@")
-  print("4)Recepcion")
+
+    def turno(self):
+        self.limpiar_area_dinamica()
+        tk.Label(self.area_dinamica, text="TURNO DEL TRABAJADOR", font=("Arial", 12)).pack(pady=10)
+
+        tk.Label(self.area_dinamica, text="Número de trabajador:").pack()
+        num_trab_entry = tk.Entry(self.area_dinamica)
+        num_trab_entry.pack()
+
+        tk.Label(self.area_dinamica, text="Selecciona el turno:").pack()
+        turno_combo = ttk.Combobox(self.area_dinamica, values=["Matutino", "Tarde", "Nocturno"])
+        turno_combo.pack()
+
+    def guardar_turno():
+        numero = num_trab_entry.get()
+        turno = turno_combo.get()
+        tk.Button(self.area_dinamica, text="Guardar turno", command=guardar_turno, bg="pink").pack(pady=15)
+
+
+    def faltas(self):
+       self.limpiar_area_dinamica()
+       tk.Label(self.area_dinamica, text=
+        
   
-  puestos=input("Selecciona el puesto del trabajador(1-4): ")
-  if puestos=="1":
-      print("El trbajador tiene el puesto de Doctor")
-  elif puestos=="2":
-      print("El trabajador tiene el puesto en pediatría ")
-  elif puestos=="3":
-      print("El trabajador tiene puesto en enfermería")
-  elif puestos=="4":
-      print("El trabador tiene puesto en recepcion recepcion")
-  else:
-    print("ERROR,NO SE ENCUENTRA OTRO PUESTO")
 
-  print("\n ----CONTROL DE ASISTENCIAS----")
-  print("Asitencias de dias por semana")
-  print("1)Un dia,esta semana")
-  print("2)Dos dias,esta semana")
-  print("3)Tres dias,esta semana")
-  print("4)Cuatro dias,esta semana")
-  print("5)Asisstió la semana completa")
+             
 
-  asistencias = input("¿Cuántos días a la semana asistió el trabajador? (1-5): ")
 
-  if asistencias == '1':
-        print("El trabajador solo asistió UN día.")
-  elif asistencias == '2':
-        print("El trabajador solo asistió DOS días.")
-  elif asistencias == '3':
-        print("El trabajador solo asistió TRES días.")
-  elif asistencias == '4':
-        print("El trabajador solo asistió CUATRO días.")
-  elif asistencias == '5':
-        print("El trabajador asistió la semana completa.")
-  else:
-        print("Opción inválida.")
-  print("\n ---PERIODO VACACIONAL---")
-  print("¿Cual es el periodo vacacional seleccionado para el trabajador?")
-  print("1)Enero-Febrero")
-  print("2)Febrero-Marzo")
-  print("3)Marzo-Abril")
-  print("4)Abril-Mayo")
-  print("5)Mayo-Junio")
-  print("6)Junio-Julio")
-  print("7)Julio-Agosto")
-  print("8)Agosto-Septiembre")
-  print("9)Septiembre-Octumbre")
-  print("10)Octubre-Noviembre")
-  print("11)Noviembre-Diciembre")
+  def imprimir(self):
+      self.limpiar_area_dinamica()
+      tk.Label(self.area_dinamica, text="LISTA DE TRABAJADORES", font=("Arial", 14)).pack(pady=10)
 
-  periodo=input("Seleccione el periodo vacacional")
-  if periodo=="1":
-    print("El trabajador tiene permitido el perido vacacional ENERO-FEBRERO")
-  elif periodo=="2":
-    print("El trabajador tiene permitido el periodo vacacional FEBRERO-MARZO")
-  elif periodo=="3":
-     print("El trabajador tiene permitido el periodo vacacional MARZO-ABRIL")
-  elif periodo=="4":
-     print("El trabajador tiene permitido el periodo vacacional ABRIL-MAYO")
-  elif periodo=="5":
-     print("El trabajador tiene permitido el periodo vacacional MAYO-JUNIO")
-  elif periodo=="6":
-     print("El trabajador tiene permitido el periodo vacacional JUNIO-JULIO")
-  elif periodo=="7":
-    print("El trabajador tiene permitido el periodo vacacional JULIO-AGOSTO")
-  elif periodo=="8":
-     print("El trabajador tiene permitido el periodo vacacional AGOSTO-SEPTIEMBRE")
-  elif periodo=="9":
-     print("El trabajador tiene permitido el periodo vacacional SEPTIEMBRE-OCTUBRE")
-  elif periodo=="10":
-     print("El trabajador tiene permitido el periodo vacacional OCTUBRE-NOVIMEMBRE")
-  elif periodo=="11":
-     print("El trabajador tiene el periodo vacacional NOVIEMBRE.DICIEMBRE")
-  else:
-     print("Periodo NO valido")
+  if not self.datos_trabajadores:
+     tk.Label(self.area_dinamica, text="Aún no hay datos").pack()
+   return
 
-control_asistencia_trabajador()
+   for persona in self.datos_trabajadores:
+       turno = "Sin turno"
+   for t in self.turnos:
+   if t["numero_trabajador"] == persona["numero_trabajador"]:
+       turno = t["turno"]
+   break
+
+    texto = f"{persona['nombre']} | Edad: {persona['edad']} | N°: {persona['numero_trabajador']} | Género: {persona['genero']} | Turno: {turno}"
+    tk.Label(self.area_dinamica, text=texto, bg="white").pack(anchor="w", padx=10, pady=3)
+
+if __name__ == "__main__":
+    ventana = tk.Tk()
+    app = Aplicacion(ventana)
+    ventana.mainloop()
 
